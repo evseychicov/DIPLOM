92ebe260b531b2c79945385d3822b451473555fb
evseychicov <evseychicov@mail.ru>
2019-04-28 11:40:50 +0300

    cookie delete

--- js/check_cookie.js
+++ js/check_cookie.js
@@ -18,4 +18,46 @@ $(document).ready(function(){
         $(".form_entry").css("display", "block");
     } 
 
+    $("#out").on("click", function(){
+        var exit = deleteCookie("Email");
+        location.reload();
+      })
+
 })
+
+
+function deleteCookie(email) {
+    setCookie(email, "", {
+      expires: -1
+    })
+  }
+
+
+  function setCookie(email, result, options) {
+    options = options || {};
+  
+    var expires = options.expires;
+  
+    if (typeof expires == "number" && expires) {
+      var d = new Date();
+      d.setTime(d.getTime() + expires * 1000);
+      expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+      options.expires = expires.toUTCString();
+    }
+  
+    result = encodeURIComponent(result);
+  
+    var updatedCookie = email + "=" + result;
+  
+    for (var propName in options) {
+      updatedCookie += "; " + propName;
+      var propValue = options[propName];
+      if (propValue !== true) {
+        updatedCookie += "=" + propValue;
+      }
+    }
+  
+    document.cookie = updatedCookie;
+  }
